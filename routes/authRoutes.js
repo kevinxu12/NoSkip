f6525caa3aaea42ae5f6810c21280a457d095035
Kevin Xu <xukevinwork@gmail.com>
2019-09-02 15:05:02 -0400

    successful running login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,15 +12,18 @@ app.get(
     )
 );
 
-app.get('/auth/google/callback', passport.authenticate('google'));
+app.get('/auth/google/callback', 
+passport.authenticate('google'),
+(req, res) => {
+    res.redirect('/surveys');
+});
 
 app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
 }); 
 
 app.get('/api/current_user', (req, res) => {
-    console.log(req);
     res.send(req.user);
   });
 };
