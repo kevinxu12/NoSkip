755f8a79fe916c539668b00afe08c549c2c08463
Kevin Xu <xukevinwork@gmail.com>
2019-09-04 20:34:36 -0400

    debug 4

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,46 +1,28 @@
 
 const passport = require('passport');
-const base64url = require('base64url');
+
 module.exports = app => {
-app.get(
-    '/auth/google', 
-    passport.authenticate(
-        'google', 
-        {
-            scope: ['profile', 'email']
-        }
-    )
-);
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
 
-app.get(
-    '/auth/google/grader', 
-    passport.authenticate(
-        'google', 
-        {
-            scope: ['profile', 'email'],
-            state: base64url(JSON.stringify({blah: 'test'}))
-        }
-    )
-);
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
 
-app.get('/auth/google/callback', 
-passport.authenticate('google'),
-(req, res) => {
-    // if(req.user.graderID) {
-    //     res.redirect('/graders');
-    // } else {
-    //     res.redirect('/surveys');
-    // }
-    res.redirect('/surveys');
-});
-
-
-app.get('/api/logout', (req, res) => {
+  app.get('/api/logout', (req, res) => {
     req.logout();
     res.redirect('/');
-}); 
+  });
 
-app.get('/api/current_user', (req, res) => {
+  app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
 };
--- services/passport.js
+++ services/passport.js
@@ -1,51 +1,37 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
 const mongoose = require('mongoose');
+const keys = require('../config/keys');
 
 const User = mongoose.model('users');
-const Grader = mongoose.model('graders');
-let callBackURL = '/auth/google/callback';
+
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
   });
-  
-passport.deserializeUser(async (id, done) => {
-    const user = await User.findById(id);
-    if(user) {
-      done(null, user);
-    } else {
-      const grader = await Grader.findById(id);
-      done(null, grader);
-    }
-  });
+});
 
 passport.use(
-    new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: callBackURL,
-    proxy: true,
-    passReqToCallback: true
-}, async (req, accessToken, refreshToken, profile, done) => {
-    const isGrader = req.query.state;
-    console.log(isGrader);
-    let existingUser = null;
-    if(!isGrader) {
-      existingUser = await User.findOne({googleId : profile.id})
-      if(existingUser) {
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
         return done(null, existingUser);
       }
-      const user = await new User({ googleId: profile.id}).save();
+
+      const user = await new User({ googleId: profile.id }).save();
       done(null, user);
-    } else {
-      existingUser = await Grader.findOne({googleId : profile.id})
-      if(existingUser) {
-        return done(null, existingUser);
-      }
-      const user = await new Grader({googleId: profile.id, graderID: 'true'}).save();
-      done(null, user);
-      }
     }
-   
-}));
+  )
+);
